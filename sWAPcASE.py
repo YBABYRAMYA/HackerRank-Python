345f1c45082faff5b4ec61d66db3940399ff4b98
YBABYRAMYA <142166288+YBABYRAMYA@users.noreply.github.com>
2024-04-26 22:25:17 +0530

    Create sWAPcASE.py
    Python program for swapcase

---
+++ sWAPcASE.py
@@ -0,0 +1,14 @@
+def swap_case(s):
+    string=""
+    for i in s:
+        if i.isupper():
+            string+=i.lower()
+        else:
+            string+=i.upper()
+        
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
